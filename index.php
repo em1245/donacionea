2ebb355179710bcc13ff5141c7812078df0927c0
em1245 <A12000830@alumnos.uady.mx>
2020-06-05 23:19:49 -0500

    thereyes

--- index.php
+++ index.php
@@ -8,6 +8,12 @@ curl_setopt($ch,CURLOPT_POST,1);
 curl_setopt($ch,CURLOPT_POSTFIELDS,"cmd=_notify-validate&" . http_build_query($_POST));
 $response = curl_exec($sch);
 echo ("$response");
+echo'<script type="text/javascript">
+    alert('$response');
+    window.location.href="index.php";
+    </script>';
+
+
 echo '$response';
 echo ($response);
 curl_close($sch);
