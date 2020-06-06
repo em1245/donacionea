926125190a82a2264dd81fe211db22f9b998677f
em1245 <A12000830@alumnos.uady.mx>
2020-06-05 23:21:49 -0500

    lastcacsff

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ $ch = curl_init();
 curl_setopt($ch,CURLOPT_URL,'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr');
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
+curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
 curl_setopt($ch,CURLOPT_POST,1);
 curl_setopt($ch,CURLOPT_POSTFIELDS,"cmd=_notify-validate&" . http_build_query($_POST));
 $response = curl_exec($sch);
