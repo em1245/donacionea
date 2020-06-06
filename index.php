af7fa6ca5f7b90a19af9690c40a3c18e31e2855f
em1245 <A12000830@alumnos.uady.mx>
2020-06-05 23:14:58 -0500

    lastEstesi

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
 curl_setopt($ch,CURLOPT_POST,1);
 curl_setopt($ch,CURLOPT_POSTFIELDS,"cmd=_notify-validate&" . http_build_query($_POST));
 $response = curl_exec($sch);
+echo ("$response");
 echo '$response';
+echo ($response);
 curl_close($sch);
 file_put_contents('test.txt',$response);
 
