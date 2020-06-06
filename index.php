f142f95f44ee2d2430b49aa221d43c5726f6d073
em1245 <A12000830@alumnos.uady.mx>
2020-06-05 23:07:34 -0500

    chaning

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr');
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
+curl_setopt($ch,CURLOPT_POST,1);
+curl_setopt($ch,CURLOPT_POSTFIELDS,"cmd=_notify-validate&" . http_build_query($_POST));
+$response = curl_exec($sch);
+curl_close($sch);
+file_put_contents('test.txt',$response);
+
+
+
+?>
